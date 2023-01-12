24abd780baa6f85de7837c9fb6ce0b5ba8189c5b
Marcosdg1221 <114072151+Marcosdg1221@users.noreply.github.com>
2023-01-12 12:48:15 +0100

    Add files via upload

---
+++ BranchYMergeMarcoEloy/src/ejemplo/main.java
@@ -0,0 +1,18 @@
+package ejemplo;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+
+public class main {
+	public static void main(String[] args) {
+	Scanner all = new Scanner(System.in);
+	System.out.println("Pon un número");
+	int n=all.nextInt();
+	System.out.println("Pon otro número");
+	int m=all.nextInt();	
+	for (int posicion=0; posicion<n; posicion++){
+	System.out.print(m);
+	}	
+}
+}
